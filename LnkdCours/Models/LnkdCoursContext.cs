0596165a8d9637f3c7210033adc5e4c4de23bed2
Hinault <ngdonfack@gmail.com>
2019-10-14 18:40:58 -0400

    Ajout EF

---
+++ LnkdCours/Models/LnkdCoursContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LnkdCours.Models
+{
+    public class LnkdCoursContext :DbContext
+    {
+    }
+}
