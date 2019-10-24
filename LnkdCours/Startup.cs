bf33df49a9aa0f193ae21c17d0181015f5d095d4
hinault <ngdonfack@gmail.com>
2019-10-24 15:06:45 -0400

    Ajout package redis

--- LnkdCours/Startup.cs
+++ LnkdCours/Startup.cs
@@ -29,6 +29,7 @@ namespace LnkdCours
 
             services.AddDbContext<LnkdCoursContext>(options =>
                     options.UseSqlite(Configuration.GetConnectionString("LocalConnection")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
