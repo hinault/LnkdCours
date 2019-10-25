e0f66539eac9f65491adb2657758de506de224a9
hinault <ngdonfack@gmail.com>
2019-10-25 14:42:31 -0400

    AjoutKeyVault

--- LnkdCours/Program.cs
+++ LnkdCours/Program.cs
@@ -18,6 +18,22 @@ namespace LnkdCours
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    // Build the current set of configuration to load values from
+                    // JSON files and environment variables, including VaultName.
+                    var builtConfig = config.Build();
+
+                    // Use VaultName from the configuration to create the full vault URL.
+                    var vaultUrl = $"https://{builtConfig["VaultName"]}.vault.azure.net/";
+
+                    // Load all secrets from the vault into configuration. This will automatically
+                    // authenticate to the vault using a managed identity. If a managed identity
+                    // is not available, it will check if Visual Studio and/or the Azure CLI are
+                    // installed locally and see if they are configured with credentials that can
+                    // access the vault.
+                    config.AddAzureKeyVault(vaultUrl);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
