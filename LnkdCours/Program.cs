c2ec00217537b6de1d2c4155ca56d01e5ff1d23a
Hinault <ngdonfack@gmail.com>
2019-10-27 11:31:07 -0400

    Ajout KeyVault

--- LnkdCours/Program.cs
+++ LnkdCours/Program.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -18,25 +21,27 @@ namespace LnkdCours
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((context, config) =>
+            .ConfigureAppConfiguration((ctx, builder) =>
+            {
+                var keyVaultEndpoint = GetKeyVaultEndpoint();
+                if (!string.IsNullOrEmpty(keyVaultEndpoint))
                 {
-                    // Build the current set of configuration to load values from
-                    // JSON files and environment variables, including VaultName.
-                    var builtConfig = config.Build();
-
-                    // Use VaultName from the configuration to create the full vault URL.
-                    var vaultUrl = $"https://{builtConfig["VaultName"]}.vault.azure.net/";
-
-                    // Load all secrets from the vault into configuration. This will automatically
-                    // authenticate to the vault using a managed identity. If a managed identity
-                    // is not available, it will check if Visual Studio and/or the Azure CLI are
-                    // installed locally and see if they are configured with credentials that can
-                    // access the vault.
-                    config.AddAzureKeyVault(vaultUrl);
-                })
+                    var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                    var keyVaultClient = new KeyVaultClient(
+                        new KeyVaultClient.AuthenticationCallback(
+                            azureServiceTokenProvider.KeyVaultTokenCallback));
+                    builder.AddAzureKeyVault(
+                        keyVaultEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager());
+                }
+            })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+
+        private static string GetKeyVaultEndpoint() => "https://myappkeyvault.vault.azure.net";
     }
+
+
 }
